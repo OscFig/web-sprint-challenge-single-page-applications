d760d5d2a0fb4768efffbd11d56c7f0cafce00ab
Oscar Figueroa <Osc.Fig96@gmail.com>
2020-12-18 10:58:31 -0800

    Almost done

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,14 @@ export default function Home() {
     const history = useHistory();
 
     const routeToPizza = () => {
-        history.push('/');//! route to pizza form
+        history.push('/Pizza');//! route to pizza form
     }
 
     return (
         <div className='home-container'>
             <img className='homeImg' 
             src='https://unsplash.com/photos/pGA4zHvpo5E'
-            alt='pizza'/>
+            alt=''/>
             <button onClick={routeToPizza} className='pizzaButton'>Make your Pizza!</button>
         </div>
     )
